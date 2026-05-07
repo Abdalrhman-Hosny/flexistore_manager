ca347d96b30ab0f3736123c81f4ad54d504dde5d
Abdalrhman Hosny <hsnyb2762@gmail.com>
2026-05-08 01:43:20 +0300

    edit ffi_types.h file

--- lib/clients/data/ffi_types.h
+++ lib/clients/data/ffi_types.h
@@ -1,11 +1,20 @@
 #ifndef FFI_TYPES_H
 #define FFI_TYPES_H
 
-// أكواد الاستجابة الموحدة للمشروع
+// --- General Status Codes (0 to -100) ---
 #define SUCCESS 0
+#define ERR_UNKNOWN -1
+
+// --- Connection & Auth (-101 to -200) ---
 #define ERR_DATABASE_CONNECTION -101
-#define ERR_INVALID_INPUT -102
 #define ERR_UNAUTHORIZED -103
-#define ERR_CLIENT_HAS_DEBT -501  // كود خاص بمهمتك (منع حذف عميل مديون)
+
+// --- Validation & Input (-201 to -300) ---
+#define ERR_INVALID_INPUT -102
+#define ERR_REQUIRED_FIELD_MISSING -201
+
+// --- Business Logic Errors (-501+) ---
+// منع الحذف بسبب الديون (القاعدة الصارمة)
+#define ERR_CLIENT_HAS_DEBT -501  
 
 #endif
